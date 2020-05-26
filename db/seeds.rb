619b89ace28e857328b116605ed1c1ca1911d943
Sofia Tofigh <sofiatofigh0@gmail.com>
2020-05-26 18:26:46 -0400

    some change

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,16 +5,3 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-require "uri"
-require "net/http"
-
-url = URI("https://api.yelp.com/v3/businesses/search?location=New York")
-
-https = Net::HTTP.new(url.host, url.port);
-https.use_ssl = true
-
-request = Net::HTTP::Get.new(url)
-request["Authorization"] = "Bearer ZzH-C9Kb04Rq0vlnXiu6vx8FgQqsnVVBLGSTRGwSvw_U7BC2f3F5nH9qmLRcdggFwe6XRoTrD_GeIzpHxHzLbo7WGIeixB81dUX4gQJiuBYZbe7ZeKlVjKuKBD_NXnYx"
-
-response = https.request(request)
-puts response.read_body
