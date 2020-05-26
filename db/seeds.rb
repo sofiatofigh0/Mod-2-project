946c52c3703fc5e766ba4f22e2327790447479aa
David <davidspector08@gmail.com>
2020-05-26 18:24:23 -0400

    update to schema

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,6 +7,7 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 require "uri"
 require "net/http"
+require 'byebug'
 
 url = URI("https://api.yelp.com/v3/businesses/search?location=New York")
 
@@ -17,4 +18,16 @@ request = Net::HTTP::Get.new(url)
 request["Authorization"] = "Bearer ZzH-C9Kb04Rq0vlnXiu6vx8FgQqsnVVBLGSTRGwSvw_U7BC2f3F5nH9qmLRcdggFwe6XRoTrD_GeIzpHxHzLbo7WGIeixB81dUX4gQJiuBYZbe7ZeKlVjKuKBD_NXnYx"
 
 response = https.request(request)
-puts response.read_body
+restaurants_hash = response.read_body
+restaurants_hash.map do |business,content|
+content.map do |key|
+Restaurant.create(name: key[:name], address: key.location[:display_address], phone_number: key.display_phone, yelp_rating: content.rating, price_range: key.price, img_url: key.image_url) 
+        end
+        end
+# response.each do |business|
+
+
+
+# response.map do |business|
+# business.map do |content|
+
